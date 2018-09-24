005a2a8eafa06684f13660dc71f6b61699686b44
wiwium <ketanchaudhari1998@gmail.com>
2018-09-25 01:47:13 +0530

    added card above process fragment

--- app/src/main/java/audioapk/com/example/android/farmertofarmer/Farms/YourFarmFragment/FarmsAdapter.java
+++ app/src/main/java/audioapk/com/example/android/farmertofarmer/Farms/YourFarmFragment/FarmsAdapter.java
@@ -20,40 +20,39 @@ import audioapk.com.example.android.farmertofarmer.R;
 
 class FarmsAdapter extends RecyclerView.Adapter<FarmsAdapter.ViewHolder>  {
 
-    private ArrayList<FarmCard> mFarmsData;
-    private Context mContext;
+    private ArrayList<FarmCard> farmsData;
+    private Context context;
 
     FarmsAdapter(Context context, ArrayList<FarmCard> farmsData) {
-        this.mFarmsData = farmsData;
-        this.mContext = context;
+        this.farmsData = farmsData;
+        this.context = context;
     }
 
 
     @Override
     public FarmsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return new ViewHolder(LayoutInflater.from(mContext).inflate(R.layout.farm_you_farms_card, parent, false));
+        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.farm_you_farms_card, parent, false));
     }
 
     @Override
     public void onBindViewHolder(FarmsAdapter.ViewHolder holder,
                                  int position) {
-        FarmCard currentFarmCard = mFarmsData.get(position);
+        FarmCard currentFarmCard = farmsData.get(position);
 
         holder.bindTo(currentFarmCard);
     }
 
     @Override
     public int getItemCount() {
-        return mFarmsData.size();
+        return farmsData.size();
     }
 
 
     class ViewHolder extends RecyclerView.ViewHolder
             implements View.OnClickListener{
 
-        private TextView farmTitle;
+        private TextView farmTitle,landEdit,noProcess,dateEdit;
         private ImageView farmImage;
-        private TextView landEdit,noProcess,dateEdit;
 
         ViewHolder(View itemView) {
             super(itemView);
@@ -71,7 +70,7 @@ class FarmsAdapter extends RecyclerView.Adapter<FarmsAdapter.ViewHolder>  {
         void bindTo(FarmCard currentFarmCard){
             farmTitle.setText(currentFarmCard.getTitle());
             landEdit.setText(String.valueOf(currentFarmCard.getLandArea()));
-            noProcess.setText("0");
+            noProcess.setText("0"); //TODO
 
             String day_string = Integer.toString(currentFarmCard.getDay()[2]);              //day
             String month_string = Integer.toString(currentFarmCard.getDay()[1] + 1);     //month
@@ -82,18 +81,20 @@ class FarmsAdapter extends RecyclerView.Adapter<FarmsAdapter.ViewHolder>  {
 
             dateEdit.setText(dateMessage);
 
-            Glide.with(mContext).load(currentFarmCard.getImageResource()).into(farmImage);
+            Glide.with(context).load(currentFarmCard.getImageResource()).into(farmImage);
 
         }
 
         @Override
         public void onClick(View view) {
-            FarmCard currentFarmCard = mFarmsData.get(getAdapterPosition());
-            Intent detailIntent = new Intent(mContext, ProcessesMain.class);
+            FarmCard currentFarmCard = farmsData.get(getAdapterPosition());
+            Intent detailIntent = new Intent(context, ProcessesMain.class);
             detailIntent.putExtra("title", currentFarmCard.getTitle());
             detailIntent.putExtra("image_resource",
                     currentFarmCard.getImageResource());
-            mContext.startActivity(detailIntent);
+            detailIntent.putExtra("land",currentFarmCard.getLandArea());
+            detailIntent.putExtra("date",currentFarmCard.getDay());
+            context.startActivity(detailIntent);
         }
     }
 
--- app/src/main/java/audioapk/com/example/android/farmertofarmer/Processes/YourProcessesFragment/ProcessYouList.java
+++ app/src/main/java/audioapk/com/example/android/farmertofarmer/Processes/YourProcessesFragment/ProcessYouList.java
@@ -1,14 +1,20 @@
 package audioapk.com.example.android.farmertofarmer.Processes.YourProcessesFragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.LinkedList;
 
@@ -21,6 +27,10 @@ public class ProcessYouList extends Fragment {
     private RecyclerView mRecyclerView;
     private ProcessesAdapter mAdapter;
 
+    private TextView farmTitle,landEdit,noProcess,dateEdit;
+    private ImageView farmImage;
+
+
 
     public ProcessYouList() {
 
@@ -31,6 +41,51 @@ public class ProcessYouList extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view =  inflater.inflate(R.layout.process_you_processes, container, false);
+
+
+
+        CardView cardView = view.findViewById(R.id.process_farm_card);
+        farmTitle = cardView.findViewById(R.id.farm_card_title);
+        farmImage = cardView.findViewById(R.id.farm_card_image);
+        landEdit = cardView.findViewById(R.id.farm_card_land);
+        noProcess = cardView.findViewById(R.id.farm_card_no_process);
+        dateEdit = cardView.findViewById(R.id.farm_card_date);
+        Intent intent = getActivity().getIntent();
+        Bundle bundle = intent.getExtras();
+        if (intent.hasExtra("title") &&
+                intent.hasExtra("image_resource") &&
+                intent.hasExtra("land") &&
+                intent.hasExtra("date"))
+        {
+
+
+
+            assert bundle != null;
+            farmTitle.setText(bundle.getString("title"));
+            landEdit.setText(String.valueOf(bundle.getDouble("land")));
+            noProcess.setText("0");  //TODO
+
+            int [] date = bundle.getIntArray("date");
+            assert date != null;
+            String day_string = Integer.toString(date[2]);              //day
+            String month_string = Integer.toString(date[1] + 1);     //month
+            String year_string = Integer.toString(date[0]);             //year
+            String dateMessage = (day_string+
+                    "/" +  month_string +
+                    "/" + year_string);
+
+            dateEdit.setText(dateMessage);
+
+            Glide.with(getActivity()).load(bundle.getInt("image_resource")).into(farmImage);
+
+        }
+
+
+
+
+
+
+
 
 
         FloatingActionButton fab = view.findViewById(R.id.add_farm_floating_button2);
@@ -48,9 +103,7 @@ public class ProcessYouList extends Fragment {
         });
 
         // Put initial data into the word list.
-        for (int i = 0; i < 20; i++) {
-            mWordList.addLast("Word " + i);
-        }
+        initData(bundle);
 
         // Create recycler view.
         mRecyclerView = view.findViewById(R.id.recyclerView2);
@@ -64,6 +117,15 @@ public class ProcessYouList extends Fragment {
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         return view;
+    }
+
+    private void initData(Bundle bundle) {
+
+        //TODO find on sql and add
+        for (int i = 0; i < 20; i++) {
+            mWordList.addLast("Word " + i);
+        }
+
     }
 
 
